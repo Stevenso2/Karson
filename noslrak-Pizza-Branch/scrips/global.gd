3bbb4e9c5d2acb4fc10c020696c7dbef320135c3
MGPizza <125213293+MGPizza@users.noreply.github.com>
2025-05-08 11:56:44 +0200

    Fixed billy i hope
    if billy isnt fixed imma beat him up more

---
+++ noslrak-Pizza-Branch/scrips/global.gd
@@ -0,0 +1,26 @@
+extends Node
+
+var TranslationMap = {
+	"venso:grass_block" = 0,
+	"venso:cobblestone" = 1,
+	"venso:dirt" = 2,
+	"venso:ice" = 3
+}
+
+var current_Block = 0
+
+var pause = false
+
+var DEV = false
+
+signal PObj_Button
+	
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("REMOVE ON FINAL"):
+		DEV = !DEV
+	
+	if pause:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
